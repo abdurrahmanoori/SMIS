f2305504e3dc5b109f9f16e160d01a237428e8ed
ARD-Developer <abdurrahmannoori10@gmail.com>
2026-01-18 13:55:51 +0430

    ui project added

---
+++ SMIS.UI/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SMIS.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "SMIS.UI" };
+        }
+    }
+}
