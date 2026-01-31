f11ba3821179696d645557302d4b14b008efb996
ARD-Developer <abdurrahmannoori10@gmail.com>
2026-01-31 17:16:25 +0430

    Comment out shop entity classes temporarily
    - Commented out Customer, CustomerCreditAccount, and CustomerCreditTransaction entities
    - Commented out Invoice, InvoiceItem, and Payment entities
    - Commented out ShopCreditAccount and ShopCreditTransaction entities
    - Commented out StockMovement entity
    - Preparing for domain model refactoring

--- SMIS.Domain/Entities/Shop/CustomerCreditTransaction.cs
+++ SMIS.Domain/Entities/Shop/CustomerCreditTransaction.cs
@@ -1,19 +1,20 @@
-using SMIS.Domain.Common.BaseAbstract;
-using SMIS.Domain.Enums;
 
-namespace SMIS.Domain.Entities.Shopp
-{
-    public class CustomerCreditTransaction : BaseAuditableEntityWithoutName
-    {
-        public int CustomerCreditAccountId { get; set; }
-        public DateTime TransactionDate { get; set; }
-        public TransactionType TransactionType { get; set; }
-        public decimal Amount { get; set; }
-        public decimal BalanceAfter { get; set; }
-        public string Description { get; set; }
-        public string ReferenceNumber { get; set; }
+//using SMIS.Domain.Common.BaseAbstract;
+//using SMIS.Domain.Enums;
 
-        // Navigation Properties
-        public virtual CustomerCreditAccount CustomerCreditAccount { get; set; }
-    }
-}
+//namespace SMIS.Domain.Entities.Shopp
+//{
+//    public class CustomerCreditTransaction : BaseAuditableEntityWithoutName
+//    {
+//        public int CustomerCreditAccountId { get; set; }
+//        public DateTime TransactionDate { get; set; }
+//        public TransactionType TransactionType { get; set; }
+//        public decimal Amount { get; set; }
+//        public decimal BalanceAfter { get; set; }
+//        public string Description { get; set; }
+//        public string ReferenceNumber { get; set; }
+
+//        // Navigation Properties
+//        public virtual CustomerCreditAccount CustomerCreditAccount { get; set; }
+//    }
+//}
--- SMIS.Domain/Entities/Shop/InvoiceItem.cs
+++ SMIS.Domain/Entities/Shop/InvoiceItem.cs
@@ -1,19 +1,20 @@
-using SMIS.Domain.Common.BaseAbstract;
 
-namespace SMIS.Domain.Entities.Shopp
-{
-    public class InvoiceItem : BaseAuditableEntityWithoutName
-    {
-        public int InvoiceId { get; set; }
-        public int ProductId { get; set; }
-        public decimal Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
-        public decimal DiscountAmount { get; set; }
-        public decimal TaxAmount { get; set; }
-        public decimal TotalAmount { get; set; }
+//using SMIS.Domain.Common.BaseAbstract;
 
-        // Navigation Properties
-        public virtual Invoice Invoice { get; set; }
-        public virtual Product Product { get; set; }
-    }
-}
+//namespace SMIS.Domain.Entities.Shopp
+//{
+//    public class InvoiceItem : BaseAuditableEntityWithoutName
+//    {
+//        public int InvoiceId { get; set; }
+//        public int ProductId { get; set; }
+//        public decimal Quantity { get; set; }
+//        public decimal UnitPrice { get; set; }
+//        public decimal DiscountAmount { get; set; }
+//        public decimal TaxAmount { get; set; }
+//        public decimal TotalAmount { get; set; }
+
+//        // Navigation Properties
+//        public virtual Invoice Invoice { get; set; }
+//        public virtual Product Product { get; set; }
+//    }
+//}
