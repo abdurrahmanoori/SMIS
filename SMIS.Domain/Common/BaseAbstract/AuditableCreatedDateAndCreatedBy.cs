55a8e935266ea4dd1cd18eeffe7078f042d03631
ARD-Developer <abdurrahmannoori10@gmail.com>
2026-02-21 10:11:04 +0430

    feat(domain): add DateTimeService for centralized date/time handling
    Introduce DateTimeService static class to provide centralized access to date/time operations across the domain layer.
    
    - Replace DateTime.Now with DateTimeService.Now in auditable entities
    - Replace DateTime.UtcNow with DateTimeService.NowOffSet in BaseEntity
    - Remove commented-out validation code from base entity classes
    - Add SQLite database files to staging

--- SMIS.Domain/Common/BaseAbstract/AuditableCreatedDateAndCreatedBy.cs
+++ SMIS.Domain/Common/BaseAbstract/AuditableCreatedDateAndCreatedBy.cs
@@ -1,20 +1,12 @@
 ﻿using SMIS.Domain.Common.BaseAbstract;
 using SMIS.Domain.Common.Interfaces;
+using SMIS.Domain.Services;
 
 namespace SMIS.Domain.Common.BaseAbstract
 {
     public abstract class AuditableCreatedDateAndCreatedBy : IAuditableCreatedDateAndCreatedBy
     {
-        //public int Id { get; set; }
         public string? CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
-
-        //public virtual void Validate()
-        //{
-        //    if (CreatedDate == null)
-        //    {
-        //        throw new Exception("CreatedDate cannot be null.");
-        //    }
-        //}
+        public DateTime CreatedDate { get; set; } = DateTimeService.Now;
     }
 }
--- SMIS.Domain/Common/BaseAbstract/BaseAuditableEntity.cs
+++ SMIS.Domain/Common/BaseAbstract/BaseAuditableEntity.cs
@@ -1,25 +1,13 @@
 ﻿using SMIS.Domain.Common.Interfaces;
+using SMIS.Domain.Services;
 
 namespace SMIS.Domain.Common.BaseAbstract
 {
     public abstract class BaseAuditableEntity : BaseEntity, IAuditableEntity
     {
         public string? CreatedBy { get; set; }
-        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? CreatedDate { get; set; } = DateTimeService.Now;
         public string? UpdatedBy { get; set; }
-        public DateTime? UpdatedDate { get; set; } = DateTime.Now;
-
-        //public virtual void Validate()
-        //{
-        //    if (CreatedDate == null)
-        //    {
-        //        throw new Exception("CreatedDate cannot be null.");
-        //    }
-
-        //    if (UpdatedDate == null)
-        //    {
-        //        throw new Exception("UpdatedDate cannot be null.");
-        //    }
-        //}
+        public DateTime? UpdatedDate { get; set; } = DateTimeService.Now;
     }
 }
