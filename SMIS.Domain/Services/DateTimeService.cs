6ca575ec851090544c3023710c064d1afb91aa0d
Abdurrahman <Abdurrahmannoori10@gmail.com>
2026-03-22 14:32:21 +0430

    fix(domain): Change DateTimeService.UtcNow to use local time
    Changed DateTimeService.UtcNow implementation to return DateTime.Now.
    - Updated UtcNow property to return DateTime.Now instead of DateTime.UtcNow
    - Added SQLite database temporary files (shm and wal)

--- SMIS.Domain/Services/DateTimeService.cs
+++ SMIS.Domain/Services/DateTimeService.cs
@@ -3,7 +3,7 @@ namespace SMIS.Domain.Services;
 public static class DateTimeService
 {
     public static DateTime Now => DateTime.Now;
-    public static DateTime UtcNow => DateTime.UtcNow;
+    public static DateTime UtcNow => DateTime.Now;
     //public static DateTime UtcNow => DateTime.UtcNow;
     //public static DateTime UtcNowOffSet => DateTime.UtcNow;
 }
