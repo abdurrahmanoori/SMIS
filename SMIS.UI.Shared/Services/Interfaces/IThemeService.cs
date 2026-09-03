e440eed0e13d4cf4830f9d4a1300276e0400a16b
ARD-Developer <abdurrahmannoori10@gmail.com>
2026-09-03 16:25:05 +0430

    other layer remove and jsut we have api, flutter project and test if api layer

--- SMIS.UI.Shared/Services/Interfaces/IThemeService.cs
+++
@@ -1,9 +0,0 @@
-namespace SMIS.UI.Shared.Services.Interfaces;
-
-public interface IThemeService
-{
-    bool IsDarkMode { get; }
-    event Action? OnThemeChanged;
-    void SetTheme(bool isDark);
-    void Toggle();
-}
--- SMIS.UI/wwwroot/js/fileHelper.js
+++
@@ -1,3 +0,0 @@
-export function triggerFileInput(element) {
-    element.click();
-}
