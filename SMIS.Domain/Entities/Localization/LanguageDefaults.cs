a66a4e411ae210e70b1a96852aadac96658be1b9
Abdurrahman Noori <66649907+abdurrahmanoori@users.noreply.github.com>
2026-09-25 06:54:13 +0430

    poc: add Dari language constants

--- SMIS.Domain/Entities/Localization/LanguageDefaults.cs
+++ SMIS.Domain/Entities/Localization/LanguageDefaults.cs
@@ -4,4 +4,7 @@ public static class LanguageDefaults
 {
     public const string EnglishId = "22222222-0000-0000-0000-000000000001";
     public const string EnglishCode = "en";
+
+    public const string DariId = "22222222-0000-0000-0000-000000000003";
+    public const string DariCode = "fa";
 }
